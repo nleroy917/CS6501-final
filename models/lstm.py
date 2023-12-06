633ac810063a8e13aad5dd762beef96c7f26f56f
Nathan LeRoy <NLeRoy917@gmail.com>
2023-12-06 10:42:06 -0500

    add initial model

---
+++ models/lstm.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+class DNAClassifier(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int, num_classes: int):
+        super(DNAClassifier, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+    
+    def forward(self, x):
+        # Set initial hidden and cell states
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda()
+
+        # Forward propagate LSTM
+        out, _ = self.lstm(x, (h0, c0))
+
+        # Decode the hidden state of the last time step
+        out = self.fc(out[:, -1, :])
+
+        return out
