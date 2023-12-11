c295489c646b712e64883388719c9813e5ef7d71
Nathan LeRoy <NLeRoy917@gmail.com>
2023-12-11 16:53:05 -0500

    update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 mkdir -p data/
-wget -O data/human.txt "https://www.dropbox.com/scl/fi/v1f95whb68q94c7uc68l8/human.txt?rlkey=uml2n2nxhd2jtqf2c77fzmbad&dl=0"
-wget -O data/dog.txt "https://www.dropbox.com/scl/fi/hg35w74sobebvls65kwvh/dog.txt?rlkey=9r3j318nz52jp0xod4a7ojhls&dl=0"
-wget -O data/chimpanzee.txt "https://www.dropbox.com/scl/fi/3qx8ntupfqwfs5i9m6q9y/chimpanzee.txt?rlkey=3348u0zctoib360jnmpfzfsxu&dl=0"
-wget -O data/fluA.fasta "https://www.dropbox.com/scl/fi/0mqssah0fmtnfpll79erv/fluA.fasta?rlkey=ucj9tn9rfdbk81cajhnyqfx1k&dl=0"
-wget -O data/fluB.fasta "https://www.dropbox.com/scl/fi/cpe90ivxj8kairauixuoy/fluB.fasta?rlkey=auyqd4jl8j2ed501fmr04tusw&dl=0"
-wget -O data/flu.txt "https://www.dropbox.com/scl/fi/ransffqet3mlyylv2sqhe/flu.txt?rlkey=3kewxpyg8bwejcx8vw28ugpyt&dl=0"
+wget -O data/flu.fasta "https://www.dropbox.com/scl/fi/qk7if0bk5hbrelf2tpfwe/flu.fasta?rlkey=1vrctsex5juz2wu8fy4a0hcpi&dl=0"
+wget -O data/covid.fasta "https://www.dropbox.com/scl/fi/kjncejtuvssr6fagnmkkk/covid.fasta?rlkey=tad75tc6gkrwt6iolcew7r669&dl=0"
