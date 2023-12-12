487564652b4bfa326a32d7f2afb5531aedbed646
Nathan LeRoy <NLeRoy917@gmail.com>
2023-12-12 15:39:04 -0500

    log_softmax

--- dna_classification/models.py
+++ dna_classification/models.py
@@ -350,7 +350,7 @@ class DNASequenceClassifier(nn.Module):
         tokens = torch.tensor(tokens).unsqueeze(0)
         prediction = self(tokens)
         # the final layer needs to be softmaxed
-        prediction = nn.functional.softmax(prediction, dim=1)
+        prediction = nn.functional.log_softmax(prediction, dim=1)
         prediction = prediction.argmax(dim=1).item()
         prediction = self.label_map[prediction]
 
